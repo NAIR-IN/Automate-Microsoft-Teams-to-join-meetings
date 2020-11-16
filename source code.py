0294860f2538eb30df6fd550c5b781f7f381929d
NAIR-IN <71177172+NAIR-IN@users.noreply.github.com>
2020-11-16 22:22:27 +0530

    Update source code.py

--- source code.py
+++ source code.py
@@ -6,7 +6,7 @@ from pynput.keyboard import Controller
 from pynput.keyboard import Key
 import webbrowser as wb
 lst=[
-    ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjEwOWNmZDMtMmQ5MC00NDM5LWFhZWUtMTY5NjZmZjJhMGI2%40thread.v2/0?context=%7b%22Tid%22%3a%226d28e4fb-9074-4a0b-a5b8-9a89f632cc60%22%2c%22Oid%22%3a%22377bd695-6601-4d9d-9bb1-bbd5cafd3784%22%7d','01:21','01:22']
+    ['https://teams.microsoft.com/l/meetup-join/19%3ameeting_YjEwOWNmZDMtMmQ5MC00NDM5LWFhZWUtMTY5NjZmZjJhMGI2%40thread.v2/0?context=%7b%22Tid%22%3a%226d28e4fb-9074-4a0b-a5b8-9a89f632cc60%22%2c%22Oid%22%3a%22377bd695-6601-4d9d-9bb1-bbd5cafd3784%22%7d','10:24','10:26']
 #input lecture stats in form of list ......
 # ["Link","start_time","end_time"]
 # give time in 24 hrs format...
